7a7fd1bc1c06f944cbe9a32661e6a4adea7ef50d
mceylan35 <mehmetceylan2696@gmail.com>
2024-11-17 02:26:38 +0300

    add custom error middleware

---
+++ AppointmentManagementSystem.Infrastructure/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,79 @@
+﻿using AppointmentManagementSystem.Application.Common.Exceptions;
+using AppointmentManagementSystem.Domain.Common;
+using FluentValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentManagementSystem.Infrastructure.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Bir hata oluştu");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            var response = new ResultDto<bool>();
+
+            switch (ex)
+            {
+                case ValidationException validationEx:
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    response = ResultDto<bool>.Fail(validationEx.Message);
+                    break;
+
+                case NotFoundException notFoundEx:
+                    context.Response.StatusCode = StatusCodes.Status404NotFound;
+                    response = ResultDto<bool>.Fail(notFoundEx.Message);
+                    break;
+
+                case UnauthorizedAccessException:
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    response = ResultDto<bool>.Fail("Bu işlem için yetkiniz yok.");
+                    break;
+
+                default:
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    response = ResultDto<bool>.Fail("Beklenmedik bir hata oluştu.");
+                    break;
+            }
+
+            await context.Response.WriteAsJsonAsync(response);
+        }
+    }
+
+    // Extension method for easy registration
+    public static class ErrorHandlingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseErrorHandling(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ErrorHandlingMiddleware>();
+        }
+    }
+}
