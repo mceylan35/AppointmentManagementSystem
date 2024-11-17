2f686a927269eeb8cda388dc4b266caf2c183301
mceylan35 <mehmetceylan2696@gmail.com>
2024-11-17 03:50:05 +0300

    assign role security bug fix

--- AppointmentManagementSystem.Application/Features/Commands/Users/UpdateUser/UpdateUserCommandHandler.cs
+++ AppointmentManagementSystem.Application/Features/Commands/Users/UpdateUser/UpdateUserCommandHandler.cs
@@ -2,10 +2,14 @@
 using AppointmentManagementSystem.Domain.Common;
 using AppointmentManagementSystem.Domain.Entities;
 using MediatR;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,10 +18,13 @@ namespace AppointmentManagementSystem.Application.Features.Commands.Users.Update
     public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, ResultDto<bool>>
     {
         private readonly IApplicationDbContext _context;
-
-        public UpdateUserCommandHandler(IApplicationDbContext context)
+        private readonly ICurrentUser _currentUser;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public UpdateUserCommandHandler(IApplicationDbContext context, ICurrentUser currentUser, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
+            _currentUser = currentUser;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<ResultDto<bool>> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
@@ -38,6 +45,7 @@ namespace AppointmentManagementSystem.Application.Features.Commands.Users.Update
                 if (await _context.Users.AnyAsync(u => u.Email == request.Email && u.Id != request.Id))
                     return ResultDto<bool>.Fail("Bu email adresi zaten kullanılıyor.");
 
+
                 user.Username = request.Username;
                 user.Email = request.Email;
                 user.IsActive = request.IsActive;
@@ -50,7 +58,20 @@ namespace AppointmentManagementSystem.Application.Features.Commands.Users.Update
                     RoleId = roleId
                 }).ToList();
 
+                 
                 await _context.SaveChangesAsync(cancellationToken);
+
+
+
+                if (_currentUser.Id == user.Id)
+                {
+                    var principal = await CreateNewClaimsPrincipal(user);
+                    await _httpContextAccessor.HttpContext.SignInAsync(
+                        CookieAuthenticationDefaults.AuthenticationScheme,
+                        principal);
+
+                }
+
 
                 return ResultDto<bool>.Success(true,"Kullanıcı başarıyla güncellendi.");
             }
@@ -58,6 +79,30 @@ namespace AppointmentManagementSystem.Application.Features.Commands.Users.Update
             {
                 return ResultDto<bool>.Fail($"Kullanıcı güncellenirken bir hata oluştu: {ex.Message}");
             }
+        }
+
+        private async Task<ClaimsPrincipal> CreateNewClaimsPrincipal(User user)
+        {
+            var roles = await _context.Roles
+                .Where(r => user.UserRoles.Select(ur => ur.RoleId).Contains(r.Id))
+                .Select(r => r.Name)
+                .ToListAsync();
+
+            var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Username),
+            new Claim(ClaimTypes.Email, user.Email)
+        };
+
+            // Rolleri ekle
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            return new ClaimsPrincipal(identity);
         }
     }
 }
